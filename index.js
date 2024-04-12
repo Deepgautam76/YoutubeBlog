225af9f8b42491b3ceffd8135f2c8198e21f9897
kamal <kamaldeep734833@gmail.com>
2024-04-12 14:53:44 +0530

    user middleware

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const path = require("path");
 const express = require("express");
-
+const path = require("path");
 const mongoose = require("mongoose");
+const cookieParser=require("cookie-parser");
 
 const userRouter = require("./routes/user");
+const { chechForAuthenticationCookie } = require("./middlewares/authentication");
 
 const app = express();
 const PORT = 8000;
@@ -15,13 +16,21 @@ mongoose
 app.set("view engine", "ejs");
 app.set("views", path.resolve("views"));
 
-//middleware inBuild
+//middlewares
 app.use(express.urlencoded({extended: false }));
+app.use(cookieParser());
+app.use(chechForAuthenticationCookie("token"))
 
 app.get("/", (req, res) => {
-  return res.render("home");
+  // console.log(req.user)
+  return res.render("home",{
+    user:req.user,
+  });
 });
 
 app.use("/user", userRouter);
 
 app.listen(PORT, () => console.log(`Server Started at PORT :${PORT}`));
+
+
+// video 30 time 12 minute
